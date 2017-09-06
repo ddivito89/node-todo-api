4f3360ca0e4bb5451f18ccd10255d2e15325a61f
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2017-09-05 20:58:38 -0500

    add POST users/login route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,18 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+//POST /users/login {email, password}
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email','password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 //start listening
 app.listen(port, () =>{
