9b2d7e35b2eeaca2bbb1c4e3a19adf156bc6e47b
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2017-09-05 20:12:54 -0500

    hashed pw

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = 'abc123';
 
-var token = jwt.sign(data, 'dd_secret');
-console.log(token);
+bcrypt.genSalt(10, (err,salt) => {
+  bcrypt.hash(password, salt, (err,hash) => {
+    console.log(hash);
+  });
+});
+var hashedPassword = '$2a$10$Qug.BpSGMHhJLT75xCuDuex72L5SSv.KYljtYVLPWAmFmPCkZCkDS'
 
-var decoded = jwt.verify(token, 'dd_secret');
-console.log('decoded:', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, 'dd_secret');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'dd_secret');
+// console.log('decoded:', decoded);
 
 // var message = 'i am user number 1';\
 // var hash = SHA256(message.toString);
